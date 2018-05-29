81aa5fe349f534ca45a417d56e850dd05ca96b8f
GitHubBrackets <diego.bolanios@gmail.com>
2018-05-29 17:57:14 -0500

    Actualizacion
    update

--- modulopruebaback/src/main/java/com/bbva/arq/front/spring/qpri/modulopruebaback/impl/ClasePruebaBackImpl.java
+++ modulopruebaback/src/main/java/com/bbva/arq/front/spring/qpri/modulopruebaback/impl/ClasePruebaBackImpl.java
@@ -2,6 +2,6 @@ package com.bbva.arq.front.spring.qpri.modulopruebaback.impl;
 
 import com.bbva.arq.front.spring.qpri.modulopruebaback.ClasePruebaBack;
 
-public class ClasePruebaBackImpl implements ClasePruebaBack {
+public ClasePruebaBackImpl implements ClasePruebaBack {
 
 }
